eb46c4b687aa0109174cc3d1a648a159eb913ea6
ahl546@student.bham.ac.uk <ahl546@student.bham.ac.uk>
2025-11-07 17:28:10 +0000

    map,room,position tests complete

--- src/org/uob/a1/.ipynb_checkpoints/Map-checkpoint.java
+++ src/org/uob/a1/.ipynb_checkpoints/Map-checkpoint.java
@@ -1,22 +1,46 @@
 package org.uob.a1;
 
 public class Map {
-
+    final private char EMPTY = '.';
+    private int width;
+    private int height;
+    private char[][] map;
 
     public Map(int width, int height)
     {
         //this represents the rows and columns starting at the top left of the map
+        this.width = width;
+        this.height = height;
+
+        map = new char[height][width];
+        
+        for( int y = 0 ; y < height; y++){
+            for( int x = 0 ; x < width; x++){
+                map[y][x]= EMPTY;
+            }
+        }       
     }
 
-    public void placeRoom(Position pos, char symbol)
+    public void placeRoom(Position position, char symbol)
     {
+        map[position.y][position.x] = symbol;
         
     }
 
     public String display()
     {
-
-        return "";
+        if (height == 0 || width == 0)
+            return "";
+        String mapdisplay = "";
+        for( int y = 0 ; y < height; y++)
+        {
+            for( int x = 0 ; x < width; x++)
+            {
+                mapdisplay += map[y][x];
+            }
+            mapdisplay += "\n";
+        }
+        return mapdisplay;
     }
     
 }
--- src/org/uob/a1/Room.java
+++ src/org/uob/a1/Room.java
@@ -1,32 +1,43 @@
 package org.uob.a1;
 
+
+
 public class Room {
+    private Position position;
+    private String name;
+    private String description;
+    private char symbol;
+    
     public Room(String name, String description, char symbol, Position position)
     {
+        this.name = name;
+        this.description = description;
+        this.symbol = symbol;
+        this.position = position;
     }
 
     public String getName()
     {
         //display the name of each room based on position
-        return "";
+        return name;
     }
 
     public String getDescription()
     {
         //add description for each room
-        return "";
+        return description;
     }
 
     public char getSymbol()
     {
         //display symbol of the room
-        return ' ';
+        return symbol;
     }
 
     public Position getPosition()
     {
         //show position on grid
-        return null;
+        return position;
     }
 
     
