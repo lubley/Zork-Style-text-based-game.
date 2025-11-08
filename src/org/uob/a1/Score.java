0e96ca9b86f084151629830337249e1fad305504
ahl546@student.bham.ac.uk <ahl546@student.bham.ac.uk>
2025-11-08 12:51:49 +0000

    score done

--- src/org/uob/a1/Score.java
+++ src/org/uob/a1/Score.java
@@ -1,12 +1,10 @@
 package org.uob.a1;
 
 public class Score {
-    private final int PUZZLE_VALUE = 10
+    private final int PUZZLE_VALUE = 10;
     private int score;
     private int roomsVisited;
     private int puzzles;
-    private final int ROOM_VALUE = 3;
-    
     
     
     public Score(int startingScore)
@@ -20,17 +18,19 @@ public class Score {
     public void visitRoom()
     {
         roomsVisited = roomsVisited + 1;
-        score = score - ROOM_VALUE;
+        
     }
 
     public void solvePuzzle()
     {
+        puzzles = puzzles + 1;
     }
 
     public double getScore()
     {
+        
+        return score - roomsVisited + (puzzles * PUZZLE_VALUE);
         //Calculates and returns the current score. The score is calculated as the starting score minus the number of rooms visited plus the number of solved puzzles times the score per puzzle.
-        return 100;
     }
         
 }
