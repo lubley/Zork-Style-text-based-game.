60eae001ccbe824473545fa3d8b6b199575d4680
ahl546@student.bham.ac.uk <ahl546@student.bham.ac.uk>
2025-11-09 19:39:13 +0000

    more game stuff

--- src/org/uob/a1/.ipynb_checkpoints/Game-checkpoint.java
+++ src/org/uob/a1/.ipynb_checkpoints/Game-checkpoint.java
@@ -8,20 +8,38 @@ public class Game {
     private Room currentRoom;
     private Score score;
     private boolean gameOn;
+
+    private Room throneroom ;
+    private Room garden;
+    private Room northHall;
+    private Room diningHall;
+    private Room centreHall;
+    private Room southHall;
+    private Room kitchen;
+    private Room meatRoom;
+    private Room bakery;
+    private Room freezer;
     
     public static void main(String args[])
     {
         Game game = new Game();
+        game.startGame();
         game.playgame();
         
     }
 
-    public void Game()
+    public void startGame()
     {
         inventory = new Inventory();
         score = new Score(100);
         map = new Map(6,7);
         gameOn = true;
+
+        String lettuce = "lettuce";
+        String patty = "patty";
+        String fish = "fish";
+        String bun = "bun";
+        String cheese = "cheese";
 
         Room throneroom = new Room("Throne Room","A grand room with the King sitting on his Throne, you may speak to him",'T', new Position(2,1));
         Room garden = new Room("Garden","A beautiful lush garden filled with delicately grown vegetables (lettuce) and the hungry royal cat",'G',new Position(1,2));
@@ -35,7 +53,20 @@ public class Game {
         Room freezer = new Room("Freezer","A chilling freezer with cheese blocks and other cold ingredients",'F',new Position(4,4));
 
         currentRoom = throneroom;
-        //constructur method which starts the game up
+
+        map.placeRoom(throneroom.getPosition(),throneroom.getSymbol());
+        map.placeRoom(garden.getPosition(),garden.getSymbol());
+        map.placeRoom(northHall.getPosition(),northHall.getSymbol());
+        map.placeRoom(diningHall.getPosition(),diningHall.getSymbol());
+        map.placeRoom(centreHall.getPosition(),centreHall.getSymbol());
+        map.placeRoom(southHall.getPosition(),southHall.getSymbol());
+        map.placeRoom(kitchen.getPosition(),kitchen.getSymbol());
+        map.placeRoom(meatRoom.getPosition(),meatRoom.getSymbol());
+        map.placeRoom(bakery.getPosition(),bakery.getSymbol());
+        map.placeRoom(freezer.getPosition(),freezer.getSymbol());
+
+        
+        
     }
 
     public void playgame()
@@ -75,5 +106,71 @@ public class Game {
             
         }
     }
+    public void move(String input)
+    {
+        switch(currentRoom.getName())
+        {
+            case "Throne Room":
+                if(input.equals("move south"){
+                    currentRoom = northHall;
+                }
+                else{
+                    System.out.println("You cannot go that way!!");
+                }
+                break;
+            case "Garden":
+                if (input.equals("move east")) {
+                    currentRoom = northHall;
+                } 
+                else 
+                {
+                    System.out.println("You cannot go that way!!");
+                }
+                 break;
+            case "North Hall":
+                if(input.equals("move south")){
+                    currentRoom = southHall;
+                }
+                if (input.equals("move north")) {
+                    currentRoom = throneroom;
+                }
+                else if (input.equals("move south")) {
+                    currentRoom = centreHall;
+                }
+                else if (input.equals("move west")) {
+                    currentRoom = garden;
+                }
+                else if (input.equals("move east")) {
+                    currentRoom = diningHall; 
+                }
+                else {
+                    System.out.println("You can't go that way!");
+                }
+                break;
+            case "Centre Hall":
+                if (input.equals("move north")) {
+                    currentRoom = northHall;
+                else if (input.equals("move south")) {
+                    currentRoom = southHall;
+                else {
+                    System.out.println("You can't go that way!");
+                break;
+            case "South Hall":
+                if (input.equals("move north")) {
+                    currentRoom = centreHall; }
+                else if (input.equals("move south")){
+                    currentRoom = bakery; }
+                else if (input.equals("move west")) {
+                    currentRoom = meatRoom; }
+                else if (input.equals("move east")) {
+                    currentRoom = kitchen; }
+                else {
+                    System.out.println("You can't go that way!"); }
+                break;
+        }
+    }
+            
+                
+                    
 
 }
